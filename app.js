14afa526c39564ba26d815e849ef5d6ea2150528
webdevyoo <yjk7454@naver.com>
2023-12-04 22:06:12 +0900

    [Momentum01-login]: 유효성검증 로직 및 form과 submit을 통한 검증처리

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ const loginInput = document.querySelector("#login-form input")
 const loginButton = document.querySelector("#login-form button")
 
 function onLoginBtnClick() {
-  console.dir(loginInput.value)
-  console.log("click")
+  const username = loginInput.value;
+  console.log(username)
+ /*  if (username === "") {
+    alert("please write your name")    
+  } else if (username.length > 15) {
+    alert("your name is too long")    
+
+  } */
 }
 
 loginButton.addEventListener("click", onLoginBtnClick)
