44aee480a4a14dcbb6280c32f94bc0d3b2100b20
webdevyoo <yjk7454@naver.com>
2023-12-05 14:14:01 +0900

    [Momentum01-login]: anchor 태그의 이벤트 기본 동작 방지

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const loginForm = document.querySelector("#login-form")
 const loginInput = document.querySelector("#login-form input") // 가장 처음 input
+const link = document.querySelector("a")
+
 
 /**
  * form submit의 기본동작인 새로고침이 발생한다.
@@ -10,7 +12,15 @@ function onLoginSubmit(e) {
   e.preventDefault();
   console.log(e)
   console.dir(e)
-  // const username = loginInput.value;
-  // console.log(username)
 }
+
+function handleLinkClick(e) {
+  console.log(e)
+  e.preventDefault();
+}
+
+/**
+ * addEventListener의 콜백함수 호출시 발생한 이벤트 객체를 매개변수로 넘긴다.
+ */
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
