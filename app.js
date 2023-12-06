39e54b8e457bd388f231f65344dd617f07dbdd73
webdevyoo <yjk7454@naver.com>
2023-12-06 11:23:36 +0900

    localstorage활용을 통한 로그인, 로그인여부확인

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
+// const link = document.querySelector("a")
+
+/**
+ * addEventListener의 콜백함수 호출시 발생한 이벤트 객체를 매개변수로 넘긴다.
+ */
+// link.addEventListener("click", handleLinkClick);
+
 const loginForm = document.querySelector("#login-form")
 const loginInput = document.querySelector("#login-form input") // 가장 처음 input
-// const link = document.querySelector("a")
 const greeting = document.querySelector("#greeting")
 
 const HIDDEN_CLASSNAME = "hidden"
+const USERNAME_KEY = "username"
 
 /**
  * form submit의 기본동작인 새로고침이 발생한다.
@@ -14,20 +21,31 @@ function onLoginSubmit(e) {
   e.preventDefault();
   const username = loginInput.value;
   loginForm.classList.add(HIDDEN_CLASSNAME)
+  localStorage.setItem(USERNAME_KEY, username) // 브라우저에서 제공하는 로컬 저장소
+  paintGreetings(username)
+}
+
+/**
+ * #greeting 영역에 입력한 값을 출력하는 함수
+ * @param {*} username 
+ */
+function paintGreetings(username) {
   // greeting.append("Hello " + username)
   // greeting.innerText = "Hello " + username
-  localStorage.setItem("username", username)
   greeting.innerText = `Hello ${username}` // 템플릿 리터럴: `백틱` + ${플레이스 홀더}
   greeting.classList.remove(HIDDEN_CLASSNAME)
 }
 
-function handleLinkClick(e) {
-  console.log(e)
-  e.preventDefault();
-}
-
 /**
- * addEventListener의 콜백함수 호출시 발생한 이벤트 객체를 매개변수로 넘긴다.
+ * 페이지 초기 진입시 로그인 여부
  */
-loginForm.addEventListener("submit", onLoginSubmit);
-// link.addEventListener("click", handleLinkClick);
+const savedUserName = localStorage.getItem(USERNAME_KEY)
+
+if (savedUserName === null) {
+  // show the form
+  loginForm.addEventListener("submit", onLoginSubmit);
+  loginForm.classList.remove(HIDDEN_CLASSNAME)
+} else {
+  // show the greetings
+  paintGreetings(savedUserName)
+}
