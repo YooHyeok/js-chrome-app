d9046fab20af8066e4d06a6bddcde5ffc29bc327
webdevyoo <yjk7454@naver.com>
2023-12-07 11:27:13 +0900

    setTimeout, Date학습 및 setInterval 시계 만들기

--- momentum/js/clock.js
+++ momentum/js/clock.js
@@ -1,5 +1,19 @@
 const clock = document.querySelector("#clock")
-setInterval(sayHello, 1000)
-function sayHello() {
-  console.log("hello")
+
+/**
+ * [Date 객체]
+ * date.getDate(); // ~일
+ * date.getDay(); // ~요일
+ * date.getFullYear(); // ~년
+ * date.getHours(); // ~시
+ * date.getMinutes(); // ~분
+ * date.getSeconds(); // ~초
+ */
+function getClock() {
+  const date = new Date();
+  clock.innerText = `${date.getHours() >= 12 ? '[PM]' : '[AM]'}${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
 }
+
+// getClock();
+setInterval(getClock, 1000) // ms 주기로 반복해서 함수를 호출한다
+// setTimeout(getClock, 1000) // ms만큼 기다린 후 1회 함수를 호출한다..
