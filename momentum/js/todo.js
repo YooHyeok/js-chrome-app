356907b2a3e36b01cf04fe76ce3e64d02e284793
webdevyoo <yjk7454@naver.com>
2023-12-10 00:07:11 +0900

    페이지 진입시 데이터 조회 후 JSON.parse 파싱작업 및 todo데이터 초기화 -> 출력

--- momentum/js/todo.js
+++ momentum/js/todo.js
@@ -2,13 +2,16 @@ const toDoForm = document.getElementById("todo-form");
 const toDoList = document.getElementById("todo-list")
 // const toDoInput = document.querySelector("#todo-form > input")
 const toDoInput = toDoForm.querySelector("input") // "#todo-form input 혹은 #todo-form > input 과 같다"
-const toDos = [];
+
+const TODOS_KEY = "toDos"
+let toDos = [];
+
 function handleToDoSubmit(e) {
   e.preventDefault(); //form 태그 안에 들어있는 input에 enter시 작동하는 submit을 방지
   const newToDo = toDoInput.value
   paintTo(newToDo)
   toDos.push(newToDo) // 입력된 todo 배열에 추가
-  saveTodos()
+  saveToDos()
   toDoInput.value = "";
 }
 
@@ -42,12 +45,26 @@ function deleteToDo(e) {
 }
 
 /**
- * JSON.stringify() - JSON형태의 문자열로 변환하여 저장한다
+ * JSON.stringify() - 자바스크립트의 Object 혹은 Array 타입의 데이터를 JSON형태의 문자열로 변환하여 저장한다
  * [a,b,c] 형태의 배열 저장시 a,b,c로 저장되는것을 
- * ["a", "b", "c"] 형태로 저장한다.
+ * '["a", "b", "c"]' 형태로 저장한다.
  */
-function saveTodos() {
-  localStorage.setItem("toDos", JSON.stringify(toDos))
+function saveToDos() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos))
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+/**
+ * 초기화
+ * 페이지 진입시 localStorage에 저장된 toDos 데이터를 로딩
+ * 해당 데이터가 비어있지 않으면 toDo를 저장할 배열 toDos에 복사해서 덮어씌운다.
+ */
+const savedToDos = localStorage.getItem(TODOS_KEY)
+if (savedToDos !== null) {
+  const paresedToDos = JSON.parse(savedToDos) // JSON.parse를 통해 javascript 데이터 형태로 변환한다.
+  toDos = paresedToDos;
+  // paresedToDos.forEach(function(item) {paintTo(item)}); // forEach : Array의 각 item에 대해 function을 실행한다. (호출한 function에 매개변수로 item을 넘긴다.)
+  // paresedToDos.forEach((item) => paintTo(item)); // 콜백함수를 arrow 함수 형태로 사용
+  toDos.forEach(paintTo); // 자바스크립트가 자동으로 parseToDos배열의 특정 요소에 순차적으로 접근하여 콜백함수의 매개변수로 전달하기 때문에 함수명만 넣어주면 해당 함수의 매개변수로 전달해준다.
+}
